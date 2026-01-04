6e9a76db3c69876648cb593590c214fadbdfa015
Ershad Khademii <khademiershad@gmail.com>
2026-01-04 16:22:25 +0330

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   // تاریخ سازگاری
   compatibilityDate: '2026-01-02',
 
-  // ۱. فعال‌سازی اسکن خودکار کامپوننت‌ها
+  // فعال‌سازی اسکن خودکار کامپوننت‌ها
   components: {
     dirs: [
       {
@@ -12,19 +12,19 @@ export default defineNuxtConfig({
     ],
   },
 
-  // ۲. معرفی فایل CSS برای پردازش توسط Tailwind 
+  //  معرفی فایل CSS برای پردازش توسط Tailwind 
   css: ['~/assets/css/main.css'],
 
   app: {
     head: {
       link: [
-        // ۳. فونت‌ها و آیکون‌ها 
+        // فونت‌ها و آیکون‌ها 
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap' },
         { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css' }
       ]
     }
   },
 
-  // ۶. فعال‌سازی ماژول تیلویند
+  //  فعال‌سازی ماژول تیلویند
   modules: ['@nuxtjs/tailwindcss']
 })
